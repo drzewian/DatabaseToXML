3aaae7a98112c0c784abe13dab122c3a68a4a952
Tiktaalik <tiktaalik>
2016-11-13 17:50:44 +0100

    connecting texboxes added

--- DatabaseToXML/DatabaseToXML/Form1.cs
+++ DatabaseToXML/DatabaseToXML/Form1.cs
@@ -22,7 +22,7 @@ namespace DatabaseToXML
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string connectionString = "Server=localhost;Port=3306;Database=sport;Uid=root;Pwd=";            
+            string connectionString = "Server=localhost;Port=3306;Database=imprezy;Uid=root;Pwd=";            
 
             // Create and open the connection in a using block. This
             // ensures that all resources will be closed and disposed
@@ -41,10 +41,10 @@ namespace DatabaseToXML
 
                         MySqlDataAdapter adapter = new MySqlDataAdapter();
 
-                        //MySqlCommand command = new MySqlCommand("select * from kluby", connection);
-                        //command.CommandType = CommandType.Text;
-
-                        //adapter.SelectCommand = command;
+                        MySqlCommand command = new MySqlCommand("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA = 'imprezy'", connection);
+                        command.CommandType = CommandType.Text;
+                        
+                        adapter.SelectCommand = command;
 
                         DataSet dataSet = new DataSet("DateBases");
 
@@ -52,16 +52,59 @@ namespace DatabaseToXML
 
                         listBox1.Items.Add("Pobrano dane z bazydanych");
 
-                        dataGridView1.DataSource = dataSet.Tables[0].DefaultView;                        
+                        dataGridView1.DataSource = dataSet.Tables[0].DefaultView;
+                        for (int i = 0; i < dataSet.Tables[0].Rows.Count; i++)
+                            comboBox1.Items.Add(dataSet.Tables[0].Rows[i].ItemArray[0]);                                              
                     }
                     
                 }
                 catch (Exception ex)
                 {
                     listBox1.Items.Add(ex.Message);
+                }              
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string connectionString = "Server=localhost;Port=3306;Database=imprezy;Uid=root;Pwd=";
+
+            // Create and open the connection in a using block. This
+            // ensures that all resources will be closed and disposed
+            // when the code exits.            
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                // Open the connection in a try/catch block. 
+                // Create and execute the DataReader, writing the result
+                // set to the console window.
+                try
+                {
+                    connection.Open();
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        listBox1.Items.Add("Nawiązano połączenie");
+
+                        MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+                        MySqlCommand command = new MySqlCommand("SELECT * FROM " + comboBox1.SelectedItem, connection);
+                        command.CommandType = CommandType.Text;
+
+                        adapter.SelectCommand = command;
+
+                        DataSet dataSet = new DataSet("DateBases");
+
+                        adapter.Fill(dataSet);
+
+                        listBox1.Items.Add("Pobrano dane z bazydanych");
+
+                        dataGridView1.DataSource = dataSet.Tables[0].DefaultView;
+                    }
+
                 }
-                
-                               
+                catch (Exception ex)
+                {
+                    listBox1.Items.Add(ex.Message);
+                }
             }
         }
     }
